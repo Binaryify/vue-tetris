74cd8be1688dd2cf963fa2ad9c2feb64cb8c6c69
root <zhuangtongfa@qq.com>
2017-06-16 09:55:53 +0800

    upgrade to v1.0.1

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import store from './vuex/store'
 import './unit/const';
 import './control';
 import { subscribeRecord } from './unit';
-window.store=store
 subscribeRecord(store); // 将更新的状态记录到localStorage
 Vue.config.productionTip = false
 /* eslint-disable no-new */
